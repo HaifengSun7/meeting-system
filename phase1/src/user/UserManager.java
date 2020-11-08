3744647ba739e342f7e3232fb6b2f2ae2a217830
Jinyang Zhao <Jinyang.zhao@mail.utoronto.ca>
2020-11-07 23:56:12 -0500

    User

--- phase1/src/user/UserManager.java
+++ phase1/src/user/UserManager.java
@@ -3,18 +3,20 @@ package user;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
+import java.lang.Exception;
+
 
 public class UserManager {
     public Map<String, User> userMapping;
 
-    public void creatUserAccount(String userType, String username, String password){
+    public void createUserAccount(String usertype, String username, String password){
         if (userMapping.containsKey(username)) {
             System.out.println("This username has been used! Try another one!"); //TODO: throw. exception. don't println. You are not system.
         } else {
-            if (userType.equals("Speaker")) {
+            if (usertype.equals("Speaker")) {
                 Speaker newuser = new Speaker(username, password);
                 addUser(newuser);
-            } else if (userType.equals("Organizer")){
+            } else if (usertype.equals("Organizer")){
                 Organizer newuser = new Organizer(username, password);
                 addUser(newuser);
             } else {
@@ -25,7 +27,11 @@ public class UserManager {
     }
 
     private void addUser(User user){
-        userMapping.put(user.username, user);
+        userMapping.put(user.getUserName(), user);
+    }
+
+    private void deleteUser(String username){
+        userMapping.remove(username);
     }
 
     public Collection<String> getAllUsers(){
@@ -37,11 +43,11 @@ public class UserManager {
     }
 
     public String getUserType(String username){
-        return userMapping.get(username).usertype;
+        return userMapping.get(username).getUserType();
     }
 
     public ArrayList<String> getContactList(String username){
-        return userMapping.get(username).contactList;
+        return userMapping.get(username).getContactList();
     }
 
     public boolean getStatus(String username){
@@ -51,23 +57,33 @@ public class UserManager {
     public String logIn(String username, String password) {
         User user = userMapping.get(username);
         if (user.password.equals(password)){
-            user.status = true;
-            return user.usertype;
+            user.setStatus(true);
+            return user.getUserType();
         } else {
             return "Login failed!"; //TODO: Please throw an exception. PLEASE. I won't write a if condition specifically for "Login failed!"
         }
     }
 
     public void logout(User user){
-        user.status = false;
+        user.setStatus(false);
     }
 
     /**
      * make attendee become Speaker
-     * @param attendee Attendee but with String.
+     * @param attendeeName Attendee but with String.
      * @exception Exception throw an exception when necessary.
      */
-    public void becomeSpeaker(String attendee) {
-        //TODO: Remember to update their events or whatever.
+
+    public void becomeSpeaker(String attendeeName) {
+        User attendee = userMapping.get(attendeeName); //If we cannot find such attendee, throw exception
+        ArrayList<String> contactlist = attendee.getContactList();
+        ArrayList<String> signedEvent = attendee.getSignedEvent();
+        boolean status =  attendee.getStatus();
+        deleteUser(attendeeName);
+        createUserAccount("speacker", attendee.getUserName(), attendee.getPassword());
+        User speaker = userMapping.get(attendeeName);
+        speaker.setContactList(contactlist);
+        speaker.setStatus(status);
+        speaker.setSignedEvent(signedEvent);
     }
 }
