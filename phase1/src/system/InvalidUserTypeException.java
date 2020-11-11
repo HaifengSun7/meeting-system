7699358ed1245878d9bf4e7f3dee2d7147c561ae
James Chen <shaohong.chen@mail.utoronto.ca>
2020-11-12 02:29:20 +0800

    minor fixes

--- phase1/src/system/InvalidUserTypeException.java
+++
@@ -1,9 +0,0 @@
-package system;
-
-import event.InvalidUserException;
-
-public class InvalidUserTypeException extends Exception{
-    public InvalidUserTypeException(String errorMessage) {
-        super(errorMessage);
-    }
-}
