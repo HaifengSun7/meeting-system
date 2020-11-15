abbc3ea2b2138cb86cb204f1d55379820c7b246f
Jinyang Zhao <Jinyang.zhao@mail.utoronto.ca>
2020-11-14 21:45:41 -0500

    Add javaDoc to UserManager

--- phase1/src/user/Organizer.java
+++ phase1/src/user/Organizer.java
@@ -8,11 +8,21 @@ public class Organizer extends User {
         this.usertype = "Organizer";
     }
 
+    /**
+     * Get nothing!
+     *
+     * @return null.
+     */
     @Override
     public ArrayList<String> getSignedEvent() {
         return null;
     }
 
+    /**
+     * Set nothing!
+     *
+     * @param signedEvent a list of eventID in string.
+     */
     @Override
     public void setSignedEvent(ArrayList<String> signedEvent) {
     }
