0df3108b3abab31d89139c0465a1c4e1eb3ccda1
Jinyang Zhao <Jinyang.zhao@mail.utoronto.ca>
2020-11-14 21:44:13 -0500

    Add javaDoc to UserManager

--- phase1/src/user/UserManager.java
+++ phase1/src/user/UserManager.java
@@ -48,6 +48,14 @@ public class UserManager {
         }
     }
 
+    /**
+     * Create a new user account.
+     * @param usertype: User's type.
+     * @param username: User's name.
+     * @param password: User's password.
+     * @exception InvalidUsernameException throw this exception if usernsme is not qualified.
+     * @exception DuplicateUserNameException throw this exception if username has exsited.
+     */
     public void createUserAccount(String usertype, String username, String password) throws Exception {
         if (username.length() < 3) {
             throw new InvalidUsernameException("length of username should be at least 3");
@@ -76,26 +84,57 @@ public class UserManager {
         userMapping.remove(username);
     }
 
+    /**
+     * Get a new user's signed event list
+     * @param username: a User's username.
+     * @return signedEvnet: a ArrayList of string.
+     */
     public ArrayList<String> getSignedEventList(String username) {
         return userMapping.get(username).getSignedEvent();
     }
 
+    /**
+     * Get a new user's user type
+     * @param username: a User's username.
+     * @return userType: a string, uesr's type.
+     */
     public String getUserType(String username) {
         return userMapping.get(username).getUserType();
     }
 
+    /**
+     * Get a new user's contact list
+     * @param username: a User's username.
+     * @return contactList: a ArrayList of string.
+     */
     public ArrayList<String> getContactList(String username) {
         return userMapping.get(username).getContactList();
     }
 
+    /**
+     * Get a new user's status
+     * @param username: a User's username.
+     * @return boolean: User's status.
+     */
     public boolean getStatus(String username) {
         return userMapping.get(username).getStatus();
     }
 
+    /**
+     * Get a new user's password
+     * @param username: a User's username.
+     * @return password: User's password.
+     */
     public String getPassword(String username) {
         return userMapping.get(username).password;
     }
 
+    /**
+     * Log in a user, change the status of a user if he login successfully.
+     * @param username: a User's username.
+     * @param password: a User's password.
+     * @exception Exception, throw it when log in failed.
+     */
     public String logIn(String username, String password) throws Exception {
         User user = userMapping.get(username);
         if (user.password.equals(password)) {
@@ -106,6 +145,12 @@ public class UserManager {
         }
     }
 
+    /**
+     * Log out a user, change the status of a user if he logout successfully.
+     *
+     * @param username: a User's username.
+     * @exception Exception, throw it when log out failed.
+     */
     public void logout(String username) {
         try {
             User user = findUser(username);
@@ -119,6 +164,7 @@ public class UserManager {
      * make attendee become Speaker
      *
      * @param attendeeName Attendee username in String.
+     * @return a list of String which is the signed evnet list of this attendee.
      * @throws Exception throw an exception when necessary.
      */
 
@@ -140,16 +186,19 @@ public class UserManager {
         }
     }
 
+    /**
+     * get all usernames
+     *
+     * @return a collection of all username.
+     */
     public Collection<String> getAllUsernames() {
         return userMapping.keySet();
     }
 
-    public Map<String, User> getUserMapping() {
-        return userMapping;
-    }
-
     /**
-     * @return a list of speakers
+     * get all speakers!
+     *
+     * @return a Arraylist of all Speaker's name.
      */
     public ArrayList<String> getSpeakers() {
         ArrayList<String> speaker = new ArrayList<>();
@@ -162,7 +211,9 @@ public class UserManager {
     }
 
     /**
-     * @return a list of Attendees
+     * get all attendees!
+     *
+     * @return a Arraylist of all Attendee's name.
      */
     public ArrayList<String> getAttendees() {
         ArrayList<String> attendee = new ArrayList<>();
@@ -174,18 +225,36 @@ public class UserManager {
         return attendee;
     }
 
+    /**
+     * Add a new event to a user's signed event list.
+     *
+     * @param eventId: a event's id.
+     * @param username: a User's username.
+     */
     public void addSignedEvent(String eventId, String username) {
         ArrayList<String> lst = userMapping.get(username).getSignedEvent();
         lst.add(eventId);
         userMapping.get(username).setSignedEvent(lst);
     }
 
+    /**
+     * Delete a event from a user's signed event list.
+     *
+     * @param eventId: a event's id.
+     * @param username: a User's username.
+     */
     public void deleteSignedEvent(String eventId, String username) {
         ArrayList<String> lst = userMapping.get(username).getSignedEvent();
         lst.remove(eventId);
         userMapping.get(username).setSignedEvent(lst);
     }
 
+    /**
+     * Add a new contact to a user's contect list.
+     *
+     * @param contactName: another user's username.
+     * @param username: a User's username.
+     */
     public void addContactList(String contactName, String username) {
         ArrayList<String> lst = userMapping.get(username).getContactList();
         if (!(lst.contains(username))) {
@@ -194,16 +263,17 @@ public class UserManager {
         userMapping.get(username).setContactList(lst);
     }
 
+    /**
+     * Delete a exist contact from a user's contect list.
+     *
+     * @param contactName: another user's username.
+     * @param username: a User's username.
+     */
     public void deleteContactList(String contactName, String username) {
         ArrayList<String> lst = userMapping.get(username).getContactList();
         lst.remove(contactName);
         userMapping.get(username).setContactList(lst);
     }
-
-    /**
-     * @param userName username.
-     * @return the password.
-     */
 
     private User findUser(String userName) throws NoSuchUserException {
         for (String name : userMapping.keySet()) {
